4811deae577db8b54e8524f15a7b371c7823b302
scott123180 <scott123180@live.com>
2020-04-18 21:03:47 -0400

    basic verison working on youtube. adding basic versions to other sites

--- content.js
+++ content.js
@@ -1,2 +1,24 @@
+function removeById(id){
+    const elementToRemove = document.getElementById(id);
+    elementToRemove.parentNode.removeChild(elementToRemove);
+}
 
-console.log("hello from content");
+const youtube = () => {
+    console.log("hello from youtube")
+    removeById("primary");
+}
+
+const facebook = () => {
+    console.log("hello from facebook")
+    const elementToRemove = document.querySelector("div[role='main']")
+    console.log(elementToRemove);
+} 
+
+let pageMap = new Map();
+pageMap.set("https://www.youtube.com/", youtube);
+pageMap.set("https://www.facebook.com/", facebook);
+
+const myLocation = window.location.href;
+
+const filterFunction = pageMap.get(myLocation);
+filterFunction();
