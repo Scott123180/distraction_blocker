6e3ff66fa42f5b44556e9aa108f97c9072657401
scott123180 <scott123180@gmail.com>
2021-12-18 12:16:09 -0500

    adding websites to block list

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+
 function removeElement(elementToRemove){
     elementToRemove.parentNode.removeChild(elementToRemove);
 }
@@ -16,22 +17,33 @@ const facebook = () => {
     removeElement(elementToRemove);
 } 
 
+const removeBody = () => {
+    const body = document.querySelector("body");
+    removeElement(body);
+}
+
 const instagram = () => {
     const mainContent = document.querySelector("main");
     removeElement(mainContent);
     const exploreBar = document.querySelector("a[href='/explore/'");
     removeElement(exploreBar);
 } 
+    
 
 let pageMap = new Map();
 pageMap.set("https://www.youtube.com/", youtube);
 pageMap.set("https://www.facebook.com/", facebook);
+pageMap.set("https://www.reddit.com/", removeBody);
+pageMap.set("https://imgur.com/", removeBody);
+pageMap.set("https://www.tiktok.com/en/", removeBody)
+
 
 pageMap.set("https://www.instagram.com/", instagram);
 pageMap.set("https://www.instagram.com/explore/", instagram);
 
 
 const myLocation = window.location.href;
+console.log(myLocation);
 
 const filterFunction = pageMap.get(myLocation);
 filterFunction();
